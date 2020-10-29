71d50ad6cd816986c83578df82d6fd085e24621d
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2020-10-29 12:56:50 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Person {
    
@@ -65,4 +66,59 @@ public class Person {
       String mail = console.nextLine();
       return new Person(name, num, mail);
    }
+   
+   public static Person personFromFile(Scanner input){
+      String name = input.next();
+      String num = input.next();
+      String mail = input.next();
+      return new Person(name, num, mail);
+   }
+   public static Person[] medarbejdereFromFile()throws FileNotFoundException{
+      Scanner load = new Scanner(new File("medarbejdere.txt"));
+      load.useDelimiter(";");
+      Person[] medarbejdere = new Person[load.nextInt()];
+      for (int i = 0; i < medarbejdere.length; i++) {
+         medarbejdere[i] = personFromFile(load);
+      }
+      return medarbejdere;
+   }
+   
+   public static void medarbejdereToFile(Person[] medarbejdere)throws FileNotFoundException{
+      PrintStream save = new PrintStream(new File("medarbejdere.txt"));
+      save.print(medarbejdere.length + ";");
+      for(int i=0; i<medarbejdere.length; i++){
+         save.print(medarbejdere[i].saveToFile() +";");
+      }
+   }
+   
+   public static Person[] addPerson(Person[] medarbejdere){
+      Person[] temp = new Person[medarbejdere.length + 1];
+      for(int i = 0; i < medarbejdere.length; i++){
+         temp[i] = medarbejdere[i];
+      }
+      temp[temp.length - 1] = Person.opretPerson();
+      return temp;
+   }
+   
+   public static Person[] deletePerson(Person[] medarbejdere, int valg){
+      Person[] temp = new Person[medarbejdere.length - 1];
+      for(int i = 0; i<valg-1; i++){
+         temp[i] = medarbejdere[i];
+      }
+      for(int i = valg + 1; i < medarbejdere.length; i++){
+         temp[i-1] = medarbejdere[i];
+      }
+      return temp;
+   }
+   
+   public static Person personFraListe(Person[] personer){
+      Scanner console = new Scanner(System.in);
+      for (int i = 0; i<personer.length; i++){
+         System.out.println("(" + (i+1) + ") " + personer[i].navn);
+      }
+      System.out.println("Vælg medarbejder: ");
+      int valg = console.nextInt();
+      return personer[valg-1];
+   }
+   
 }
