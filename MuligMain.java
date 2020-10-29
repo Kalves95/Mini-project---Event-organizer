a6fc85d6aafddb33e6868d919c6248fb27c26dc1
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2020-10-29 13:56:13 +0100

    Update MuligMain.java

--- MuligMain.java
+++ MuligMain.java
@@ -1,41 +1,42 @@
 import java.util.*;
 import java.io.*;
 
-public class MuligMain { 
+public class MuligArrangementFordeling {
    public static void main (String[] args) throws FileNotFoundException {
       Scanner der = new Scanner(System.in);
       Person[] medarbejdere = Person.medarbejdereFromFile();
       Virksomhed[] kunder = Virksomhed.kunderFromFile();
-      Arrangement arr = new Arrangement();
+      Arrangement a1 = new Arrangement();
+      
       //Velkomst menu, med valg muligheder.
-      System.out.printf("%27s %n", "Velkommen til PlanOrgan");
+      System.out.printf("%27S %n", "Velkommen til PlanOrgan");
       System.out.println();
       int svar = 1;
-      //Person p1 = new Person();
-      //Virksomhed v1 = new Virksomhed();
+      Person p1 = new Person();
+      Virksomhed v1 = new Virksomhed();
             
-      while(svar != 4) {
+      while(svar != 5) {
          System.out.printf("%s %n", "Tast 1 for person menu.");
          System.out.printf("%s %n", "Tast 2 for kunde menu.");
          System.out.printf("%s %n", "Tast 3 for arrangement menu.");
-         System.out.printf("%s %n", "Tast 4 for at afslutte. ");
+         System.out.printf("%s %n", "Tast 4 for at bruge søgefunktionen");
+         System.out.printf("%s %n", "Tast 5 for at afslutte. ");
          svar = der.nextInt();
 
       //Vores switch menu.
       switch(svar) {
+         
          // Person menu
          case 1:
             System.out.println("Du har valgt at oprette en ny person");
             System.out.println("------------------------------------");
-            medarbejdere = Person.addPerson(medarbejdere);
-            Person.medarbejdereToFile(medarbejdere);
+            p1 = Person.opretPerson();
             //System.out.println(p1);
             System.out.println("------------------------------------");
                break;
+         
          //Virksomhed / kunde menu.   
          case 2:
-            System.out.println("Du har valgt kunde menuen.");
-            System.out.println("---------------------------");
             System.out.println("Tast 1 hvis du vil oprette en privat kunde.");
             System.out.println("Tast 2 hvis du vil oprette erhvervs kunde.");
             System.out.println("Tast 3 hvis du vil oprette forenings kunde.");
@@ -44,24 +45,21 @@ public class MuligMain {
             if(svar1 == 1) {
                System.out.println("Du har valgt at oprette en privat kunde");
                System.out.println("---------------------------------------");
-               kunder = Virksomhed.addVirksomhed(kunder);
-               Virksomhed.kunderToFile(kunder);
+               Person pr2 = Person.opretPerson();
                //System.out.println(pr2);
                System.out.println("---------------------------------------");
             
             } if(svar1 == 2) {
                System.out.println("Du har valgt at oprette en erhvervs kunde.");
                System.out.println("------------------------------------------");
-               kunder = Virksomhed.addVirksomhed(kunder);
-               Virksomhed.kunderToFile(kunder);
+               Person er1 = Person.opretPersonCVR();               
                //System.out.println(v1);
                System.out.println("------------------------------------------");
                            
             } if(svar1 == 3) {
                System.out.println("Du har valgt at oprette en forenings kunde.");
                System.out.println("-------------------------------------------");
-               kunder = Virksomhed.addVirksomhed(kunder);
-               Virksomhed.kunderToFile(kunder);
+               Person fo2 = Person.opretPersonForening();
                //System.out.println(fo2);
                System.out.println("-------------------------------------------");
             }
@@ -70,77 +68,112 @@ public class MuligMain {
          //Vores arrangement menu
          case 3:
             System.out.println("Tast 1 hvis du vil oprette et arrangement.");
-            System.out.println("Tast 2 hvis du vil oprette et event.");
-            System.out.println("Tast 3 hvis du vil redigere et arrangement.");
-            System.out.println("Tast 4 hvis du vil slette arrangement");
-            System.out.println("Tast 5 hvis du vil åbne arrangement fil");
+            System.out.println("Tast 2 hvis du vil redigere et arrangement / event.");
+            System.out.println("Tast 3 hvis du vil åbne arrangement fil");
             int svar2 = der.nextInt();
             
             if(svar2 == 1) {
-               System.out.println("Du har valgt at oprette et arrengement.");
+               System.out.println("Du har valgt at oprette et arrengement."); // HER HER HER
                System.out.println("---------------------------------------");
-               System.out.println("Vælg kunde: ");
-               Virksomhed kunde = Virksomhed.virksomhedFraListe(kunder);
-               arr = Arrangement.opretArrangement(kunde, medarbejdere);
-               arr.saveArrangementToFile();
-               System.out.println();
+               a1 = Arrangement.opretArrangement(v1);//
+               a1.saveArrangementToFile();
                System.out.println("---------------------------------------");
-               
             
             }if(svar2 == 2) {
-               System.out.println("Du har valgt at oprette et event.");
-               System.out.println("--------------------------------");
-              
-               //Event e1 = Event.opretEvent(v1, p1);
-               
-               //System.out.println(e1);
-               System.out.println("--------------------------------");
-            
-            }if(svar2 == 3) {
                System.out.println("Du har valgt at redigere i et arrangement.");
                System.out.println("------------------------------------------");
-               arr = Arrangement.loadArrangementFromFile();
-               arr.redigerArrangement(medarbejdere);
-            
-            }if(svar2 == 4) {
-               System.out.println("Du har valgt at slette et arrangement.");
-               System.out.println("--------------------------------------");
+               System.out.println("Tast 1 hvis du vil redigere i et arrangement");
+               System.out.println("Tast 2 hvis du vil oprette et event.");
+               System.out.println("Tast 3 hvis du vil slette arrangement");
+               System.out.println("Tast 4 hvis du ønsker at redigere et event");
+
+               int svar3 = der.nextInt();
                
-               sletFil();
+               switch(svar3) {
+                  
+                  case 1: // HER
+                     System.out.println("Du har valgt at redigere et arrangement.");
+                     System.out.println("----------------------------------------");
+                     //a1 = Arrangement.opretArrangement(v1);
+                     a1.redigerArrangement();
+                     System.out.println("----------------------------------------");
+                     break;
+                  
+                  case 2:
+                     System.out.println("Du har valgt at oprette et event.");
+                     System.out.println("---------------------------------");
+                     
+                     Event e1 = Event.opretEvent(v1, p1);
                
-               System.out.println("-------------------------------------------------------------------");
+                     System.out.println(e1);
+                     System.out.println("------------------------------------");
+                     break;
+                     
+                  case 3:
+                     System.out.println("Du har valgt at slette et arrangement.");
+                     System.out.println("--------------------------------------");
+                     
+                     sletFil();
+                     System.out.println("-------------------------------------------------------------------");
+                     break;
+                     
+                  case 4: // HER
+                     System.out.println("Du har valgt at redigere et event.");
+                     System.out.println("----------------------------------");
+                     Event e2 = Event.opretEvent(v1, p1);
+                     
+                     e2.redigerEvent();
+                  
+                  default:
+                     System.out.println("Velkommen til hovedmenuen");
+                     System.out.println("-------------------------");
+
+               } 
             
-            }if(svar2 == 5) {
+            }if(svar2 == 3) {
                System.out.println("Du har valgt at åbne en arrangement fil");
                System.out.println("---------------------------------------");
                
                Arrangement bryllup = Arrangement.loadArrangementFromFile();
       
-               Virksomhed sted = Virksomhed.opretVirksomhed();
-               Person ansat = Person.opretPerson();
+               //Virksomhed sted = Virksomhed.opretVirksomhed();
+               //Person ansat = Person.opretPerson();
       
-               bryllup.addEvent(sted, ansat);
+               //bryllup.addEvent(sted, ansat);
       
-               bryllup.saveArrangementToFile();
+               //bryllup.saveArrangementToFile();
                
                System.out.println("---------------------------------------");
             }   
                break;
+               
+            case 4: // HER
+               System.out.println("Du har valgt at bruge søgefunktionen.");
+               System.out.println("-------------------------------------");
+               System.out.println("Tast 1 hvis du vil søge i arrayet.");
+               int svar4 = der.nextInt();
+               
+               if(svar4 == 1) {
+               
+               Arrangement a2 = Arrangement.loadArrangementFromFile();
+               a2.searchArray();
+               System.out.println("-------------------------------------");
+            }
             
-            case 4:
+            case 5:
                break;
          
-            default:
-               System.out.println("Indtast venligst et gyldigt nummer, tak.");
+         default:
+            System.out.println("Indtast venligst et gyldigt nummer, tak.");
       }
       
      }
-     Person.medarbejdereToFile(medarbejdere);
-     Virksomhed.kunderToFile(kunder);
-     System.out.println("Du har valgt at afslutte. Fortsat god dag!");
-   } // Her slutter main. 
+     System.out.println("Du har valgt at afslutte, fortsat god dag.");
+     System.out.println("------------------------------------------");
    
-   public static void sletFil() {
+   }
+   
+    public static void sletFil() {
       Scanner in = new Scanner(System.in);
       System.out.println("Hvilken fil skal slettes?");
       String name = in.nextLine();
@@ -158,5 +191,6 @@ public class MuligMain {
       }
 
    }
- 
+
+
 }
